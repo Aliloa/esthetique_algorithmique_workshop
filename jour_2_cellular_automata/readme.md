af3e03602ba7c9e3d4598ed5053d5a55756ce44e
Alina <108938418+Aliloa@users.noreply.github.com>
2026-01-13 15:39:18 +0100

    mini jeu

--- jour_2_cellular_automata/readme.md
+++ jour_2_cellular_automata/readme.md
@@ -1,5 +1,21 @@
+# Présentation
+- Projet réalisé en JavaScript avec la bibliothèque p5.js
 
-crédits:
+Le projet consiste en un petit jeu 2D pixelisé appelé The Floor Is Lava.
+Une map est générée procéduralement à l’aide d’un automate cellulaire.
+Le joueur peut se déplacer sur les cases libres pendant que le feu (également généré sur le principe d'automate cellulaire) se propage progressivement sur la map.
+
+Le but est de survivre le plus longtemps possible sans toucher le feu.
+
+## Installation et exécution
+
+- Ouvrir le fichier index.html
+
+### crédits:
 - https://www.youtube.com/watch?v=FSNUp_8Xvqo
+
 - https://www.roguebasin.com/index.php/Cellular_Automata_Method_for_Generating_Random_Cave-Like_Levels
+
 - https://p5js.org/examples/math-and-physics-game-of-life/
+
+- uiverse.io pour le style du bouton
--- jour_2_cellular_automata/script.js
+++ jour_2_cellular_automata/script.js
@@ -1,48 +1,19 @@
-//-----------------JEU
-
-let player = {
-    x: 25,
-    y: 25,
-    color: "red"
-};
-
-//spawn le joueur dans une case 0
-function spawnPlayer() {
-    for (let y = 25; y < rows; y++) {
-        for (let x = 25; x < cols; x++) {
-            if (map[y][x] == 0) { // case vide
-                player.x = x;
-                player.y = y;
-                return;
-            }
-        }
-    }
-}
-
-document.addEventListener("keydown", function (e) {
-    let newX = player.x;
-    let newY = player.y;
-
-    if (e.key === "ArrowLeft") newX--;
-    else if (e.key === "ArrowRight") newX++;
-    else if (e.key === "ArrowUp") newY--;
-    else if (e.key === "ArrowDown") newY++;
-
-    // vérifier si c'est pas un mur
-    if (map[newY] && map[newY][newX] == 0) {
-        player.x = newX;
-        player.y = newY;
-    }
-})
-
 //--------------------GENERATION DE LA MAP
-let cols = 50; // nombre de colonnes
-let rows = 50; // nombre de lignes
-let cell_size = 10; // taille d'une case
+let cols = 50;
+let rows = 30; //ne pas mettre de nombre paire sinon les math floor /2 se cassent
+let cell_size = 15; // taille d'une case
 let map = [];
 
 function setup() {
+    cols = Math.floor(windowWidth / cell_size);
     createCanvas(windowWidth, rows * cell_size);
+    fire = [];
+    for (let y = 0; y < rows; y++) {
+        fire[y] = [];
+        for (let x = 0; x < cols; x++) {
+            fire[y][x] = 0;
+        }
+    }
 }
 
 function play() {
@@ -59,7 +30,6 @@ function play() {
     for (let i = 0; i < 5; i++) {
         map = automate(map);
     }
-
     redraw();
 }
 
@@ -74,26 +44,30 @@ function draw() {
             rect(x * cell_size, y * cell_size, cell_size, cell_size);
         }
     }
+
+    spawnFire();
+    spreadFire();
+
     // dessiner le perso
     fill(player.color);
     rect(player.x * cell_size, player.y * cell_size, cell_size, cell_size);
 }
 
 //automate cellulaire
-function automate(oldMap) {
-    let newMap = [];
+function automate(old_map) {
+    let new_map = [];
     for (let y = 0; y < rows; y++) {
-        newMap[y] = [];
+        new_map[y] = [];
         for (let x = 0; x < cols; x++) {
-            let neighbors = countWallsAround(oldMap, x, y);
-            if (oldMap[y][x] == 1) {
-                newMap[y][x] = neighbors >= 4 ? 1 : 0;
+            let neighbors = countWallsAround(old_map, x, y);
+            if (old_map[y][x] == 1) {
+                new_map[y][x] = neighbors >= 4 ? 1 : 0;
             } else {
-                newMap[y][x] = neighbors >= 5 ? 1 : 0;
+                new_map[y][x] = neighbors >= 5 ? 1 : 0;
             }
         }
     }
-    return newMap;
+    return new_map;
 }
 
 // compter les murs autour d'une cellule
@@ -121,4 +95,136 @@ let start = document.querySelector(".start");
 start.addEventListener("click", function () {
     play();
     spawnPlayer();
+
+    fire = [];
+    for (let y = 0; y < rows; y++) {
+        fire[y] = [];
+        for (let x = 0; x < cols; x++) {
+            fire[y][x] = 0;
+        }
+    }
+    document.querySelector(".game-over").style.display = "none";
+    startTimer();
+
 });
+
+//-----------------JEU
+
+let player = {
+    x: cols / 2,
+    y: rows / 2,
+    color: "blue"
+};
+
+//spawn le joueur dans une case 0
+function spawnPlayer() {
+    let startX = Math.floor(cols / 2);
+    let startY = Math.floor(rows / 2);
+
+    for (let y = startY; y < rows; y++) {
+        for (let x = startX; x < cols; x++) {
+            if (map[y][x] == 0) { // case vide
+                player.x = x;
+                player.y = y;
+                return;
+            }
+        }
+    }
+}
+
+document.addEventListener("keydown", function (e) {
+    let newX = player.x;
+    let newY = player.y;
+
+    if (e.key === "ArrowLeft") newX--;
+    else if (e.key === "ArrowRight") newX++;
+    else if (e.key === "ArrowUp") newY--;
+    else if (e.key === "ArrowDown") newY++;
+
+    // vérifier si c'est pas un mur
+    if (map[newY] && map[newY][newX] == 0) {
+        player.x = newX;
+        player.y = newY;
+    }
+})
+
+//------------------------------FEU
+
+function spawnFire() {
+    for (let y = 10; y < rows; y++) {
+        for (let x = 10; x < cols; x++) {
+            if (map[y][x] == 0) {
+                fire[y][x] = 1;
+                return;
+            }
+        }
+    }
+}
+
+function spreadFire() {
+
+    let new_fire = fire.map(row => [...row]);
+
+    for (let y = 0; y < rows; y++) {
+        for (let x = 0; x < cols; x++) {
+            if (fire[y][x] == 1) {
+                // cases à côté
+                let neighbors = [
+                    [x + 1, y],
+                    [x - 1, y],
+                    [x, y + 1],
+                    [x, y - 1]
+                ];
+
+                for (let [nx, ny] of neighbors) {
+                    if (nx >= 0 && ny >= 0 && nx < cols && ny < rows) {
+                        if (map[ny][nx] == 0 && fire[ny][nx] == 0 && random() < 0.1) {//probabilité de se propager à la prochaine case (ralentir)
+                            new_fire[ny][nx] = 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    let colors = ["#FF4500", "#FF8C00", "#FFA500"];
+    for (let y = 0; y < rows; y++) {
+        for (let x = 0; x < cols; x++) {
+            if (fire[y][x] === 1) {
+                let c = random(colors);
+                fill(c);
+                rect(x * cell_size, y * cell_size, cell_size, cell_size);
+            }
+        }
+    }
+
+    fire = new_fire;
+
+    // le joueur touche le feu
+    if (fire[player.y][player.x] == 1) {
+        console.log("Game Over");
+        document.querySelector(".game-over").style.display = "flex";
+        document.querySelector(".final-time").innerHTML = time;
+        stopTimer();
+    }
+}
+
+//----------------TIMER
+
+let time = 0;
+let timer_interval = null;
+
+function startTimer() {
+    stopTimer();
+    time = 0;
+    document.querySelector(".time").textContent = time;
+
+    timer_interval = setInterval(() => {
+        time++;
+        document.querySelector(".time").textContent = time;
+    }, 1000);
+}
+
+function stopTimer() {
+    clearInterval(timer_interval);
+}
